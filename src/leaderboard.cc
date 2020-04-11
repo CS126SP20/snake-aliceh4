dad436c04a4ad84dae7172ba68749079fda3a409
Alice Huang <aliceh4@illinois.edu>
2020-04-11 16:20:59 -0400

    Add query to AddScoreToLeaderBoard and begin working on RetrieveHighScores.

--- src/leaderboard.cc
+++ src/leaderboard.cc
@@ -15,20 +15,28 @@ using std::vector;
 // See examples: https://github.com/SqliteModernCpp/sqlite_modern_cpp/tree/dev
 
 LeaderBoard::LeaderBoard(const string& db_path) : db_{db_path} {
-  db_ << "CREATE TABLE if not exists leaderboard (\n"
-         "  name  TEXT NOT NULL,\n"
-         "  score INTEGER NOT NULL\n"
-         ");";
+  try {
+    db_ << "CREATE TABLE if not exists leaderboard (\n"
+           "  name  TEXT NOT NULL,\n"
+           "  score INTEGER NOT NULL\n"
+           ");";
+  } catch (const std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
 }
 
 void LeaderBoard::AddScoreToLeaderBoard(const Player& player) {
-  // TODO(you): Add your query here.
-  db_ << "";
+  try {
+    db_ << "Insert into leaderboard (name, score) values (?, ?);"
+        << player.name
+        << player.score;
+  } catch (const std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
 }
 
 vector<Player> GetPlayers(sqlite::database_binder* rows) {
   vector<Player> players;
-
   for (auto&& row : *rows) {
     string name;
     size_t score;
@@ -41,9 +49,14 @@ vector<Player> GetPlayers(sqlite::database_binder* rows) {
 }
 
 vector<Player> LeaderBoard::RetrieveHighScores(const size_t limit) {
-  // TODO(you): Add your query here.
-  auto rows = db_ << "";
-  return GetPlayers(&rows);
+  try {
+
+    auto rows = db_ << "";
+    // TODO: How to find the top players; order by descending
+    return GetPlayers(&rows);
+  } catch (const std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
 }
 
 vector<Player> LeaderBoard::RetrieveHighScores(const Player& player,
